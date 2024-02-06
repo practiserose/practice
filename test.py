2c5a0e6086e98628425acf7dee5ff4b7760f75ec
roseatsole <okwisarose77@gmail.com>
2024-02-06 16:48:33 +0300

    first commit

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-name = input("What is your name? ")
-lbs = input("Please enter your weight in pounds " + name+ " ")
-kgs = int(lbs)*0.45
-print("Your weight is "+ kgs + "Kilograms")
+
+password = 'AishaRose@123'
+pass_attempt = 0
+pass_limit = 3
+while pass_attempt < pass_limit:
+    pas = input("Enter password: ")
+    pass_attempt += 1
+    if pas == password:
+        print("Login Successful")
+        break
+
+else:
+    print("Account blocked, contact admin for assistance")
+
+
 
